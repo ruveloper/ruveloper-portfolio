d42c3b77628b0b958665f9633577c0361743e8bd
Ruben Caballero <dev.rubencaballero@gmail.com>
2022-06-18 16:37:20 -0500

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG
+
+## [0.1.0] Start Project: Basic config + Tailwind
+
+#### Follow the README file to run the project in development mode.
+- Start project
+- Added requirements
+- Config tailwind and browser-reload.
+- Config basic url patterns, the "website" app and initial templates.
+
+
---
+++ apps/website/views/http_error_views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+
+# ------------------- ERROR VIEWS -------------------
+def error_400(request, exception=None):
+    context = {
+        'exception':exception,
+    }
+    return render(request, 'website/errors/400.html', context, status=400)
+
+
+def error_403(request, exception=None):
+    context = {
+        'exception':exception,
+    }
+
+    return render(request, 'website/errors/403.html', context, status=403)
+
+
+def error_404(request, exception=None):
+    context = {
+        'exception':exception,
+    }
+
+    return render(request, 'website/errors/404.html', context, status=404)
+
+
+def error_500(request, exception=None):
+    context = {}
+
+    return render(request, 'website/errors/500.html', context, status=500)
