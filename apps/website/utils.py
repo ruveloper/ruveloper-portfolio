02baa1dda9851239b00e2c3b6e09ab594d1862d3
Ruben Caballero <dev.rubencaballero@gmail.com>
2022-06-29 21:34:25 -0500

    Linked CMS Base model to templates

--- apps/website/utils.py
+++ apps/website/utils.py
@@ -1,10 +1,17 @@
+from typing import Type, Union
 from uuid import uuid4
 
+from django.db import models
+from solo.models import SingletonModel
 
-def upload_cms_image_location(instance, filename:str):
+
+def get_model_or_none(model: Type[models.Model]) -> Union[None, Type[models.Model]]:
+    try:
+        return model.objects.get()
+    except models.ObjectDoesNotExist:
+        return None
+
+
+def upload_cms_image_location(instance, filename: str) -> str:
     extension = filename.split('.')[-1]
     return f'cms/{uuid4().hex}.{extension}'
-
-
-
-
--- apps/website/views/page_views.py
+++ apps/website/views/page_views.py
@@ -1,22 +1,60 @@
 from django.views.generic import TemplateView
 
+from apps.website.models import Base
+
+from apps.website.utils import get_model_or_none
+
 
 class HomePage(TemplateView):
     template_name = "website/pages/home_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePage, self).get_context_data(**kwargs)
+        # ! ---- CMS Data ----
+        context['cms_base']:Base = get_model_or_none(Base)
+
+        return context
 
 
 class AboutPage(TemplateView):
     template_name = "website/pages/about_page.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(AboutPage, self).get_context_data(**kwargs)
+        # ! ---- CMS Data ----
+        context['cms_base']:Base = get_model_or_none(Base)
+
+        return context
+
 
 class ProjectsPage(TemplateView):
     template_name = 'website/pages/projects_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProjectsPage, self).get_context_data(**kwargs)
+        # ! ---- CMS Data ----
+        context['cms_base']:Base = get_model_or_none(Base)
+
+        return context
 
 
 class ProjectDetailPage(TemplateView):
     template_name = 'website/pages/project_detail_page.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(ProjectDetailPage, self).get_context_data(**kwargs)
+        # ! ---- CMS Data ----
+        context['cms_base']:Base = get_model_or_none(Base)
+
+        return context
+
 
 class ContactPage(TemplateView):
     template_name = 'website/pages/contact_page.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(ContactPage, self).get_context_data(**kwargs)
+        # ! ---- CMS Data ----
+        context['cms_base']:Base = get_model_or_none(Base)
+
+        return context
