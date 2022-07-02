59599fe3acb92c7eaa3142a10046f532e88e1e25
Ruben Caballero <dev.rubencaballero@gmail.com>
2022-07-02 18:51:47 -0500

    Admin customization for all models

--- apps/website/admin/cms_admin.py
+++ apps/website/admin/cms_admin.py
@@ -1,19 +1,29 @@
 from django.contrib import admin
-from solo.admin import SingletonModelAdmin
+from django.utils.translation import gettext_lazy as _
 
+from solo.admin import SingletonModelAdmin
 from apps.website.models import Base, Home, Project
 from apps.website.models import About, Technology, Company, ResumeEntry
 
 
 # ? --------- Base model ---------
 @admin.register(Base)
-class BaseAdmin(SingletonModelAdmin): pass
+class BaseAdmin(SingletonModelAdmin):
+    fieldsets = [
+        (None, {'fields':['email']}),
+        (_('Personal brand'), {'fields':['brand', 'logo', 'favicon']}),
+        (_('Social networks'), {'fields':['github', 'linkedin']})
+    ]
 
 
 # ? --------- Home model ---------
 @admin.register(Home)
 class HomeAdmin(SingletonModelAdmin):
     readonly_fields = ('dev_photo_webp',)
+    fieldsets = [
+        (None, {'fields':['dev_photo', 'dev_photo_webp']}),
+        (_('Card'), {'fields':['card_title', 'card_body']}),
+    ]
 
 
 # ? --------- About model ---------
@@ -35,6 +45,10 @@ class ResumeEntryInline(admin.StackedInline):
 @admin.register(About)
 class AboutAdmin(SingletonModelAdmin):
     readonly_fields = ('profile_image_webp',)
+    fieldsets = [
+        (_('Profile'), {'fields':['profile_image', 'profile_image_webp', 'body']}),
+        (_('Activate/Inactivate sections'), {'fields':['activate_stack', 'activate_trust_me', 'activate_resume']})
+    ]
     inlines = [TechnologyInline, CompanyInline, ResumeEntryInline]
 
 
@@ -42,7 +56,16 @@ class AboutAdmin(SingletonModelAdmin):
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     readonly_fields = ('cover_image_webp',)
+    fieldsets = [
+        (None, {'fields': ['title', 'slug', 'cover_image', 'cover_image_webp']}),
+        (_('Project content'), {'fields': ['description', 'detail']})
+    ]
 
 
 # ? --------- Shared models ---------
-admin.site.register(Technology)
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'logo', 'priority_order', 'description']}),
+        (_('Relations'), {'fields': ['about', 'project_entry']})
+    ]
